84f2df5e9be3eb00cc4ff79a5de52d337e953cbc
Ubaldo Taladriz <utaladriz@exe.cl>
2017-10-30 16:28:49 -0300

    actualización solo de base de datos . archivo disponible en subdirectorio actualizacion_bd

---
+++ actualizacion_bd/2017_10_30_16_25_00.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.mantenedor(
+tabla, singular, plural)
+VALUES ('categoria', 'Categoría', 'Categorías');
+INSERT INTO public.mantenedor(
+  tabla, singular, plural)
+VALUES ('nivel_digitalizacion', 'Nivel de digitalización', 'Niveles de digitalización');
